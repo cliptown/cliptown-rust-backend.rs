244cdfb451ae1a6e058e89e8568b9374a2346f1c
Alexander Mills <alex@oresoftware.com>
2026-08-03 14:26:20 -0500

    feat(DEN-1578): register MemeBank persistence entities

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -4,3 +4,5 @@ pub mod app_vault_record_head;
 pub mod clip;
 pub mod external_step_up_challenge;
 pub mod external_step_up_proof;
+pub mod memebank_transfer;
+pub mod memebank_transfer_idempotency;
